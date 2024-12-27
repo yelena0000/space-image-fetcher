39cea8777fe35716c9226e5f8c8111233a5ff61a
yelena0000 <kirillnek07081999@gmail.com>
2024-12-27 13:57:46 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,8 +2,21 @@ import os
 from urllib.parse import urlparse
 
 
+def create_folder(folder_name):
+    os.makedirs(folder_name, exist_ok=True)
+
+
 def get_file_extension(url):
     parsed_url = urlparse(url)
     file_name = os.path.basename(parsed_url.path)
     file_extension = os.path.splitext(file_name)[1]
     return file_extension
+
+
+def get_all_files(directory):
+    all_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            full_path = os.path.join(root, file)
+            all_files.append(full_path)
+    return all_files
