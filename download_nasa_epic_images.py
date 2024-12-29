fc0ffeb73a2cbf43c973cbab14c2396e1aa74820
yelena0000 <kirillnek07081999@gmail.com>
2024-12-29 17:57:50 +0300

    Update download_nasa_epic_images.py

--- download_nasa_epic_images.py
+++ download_nasa_epic_images.py
@@ -1,46 +1,37 @@
 import argparse
-import os
 import requests
 from environs import Env
 
 from utils import create_folder
+from utils import download_and_save_image
 
 
 def download_nasa_epic_images(api_key, folder_name, count=10):
     create_folder(folder_name)
+
     api_url = 'https://api.nasa.gov/EPIC/api/natural/images'
+    params = {'api_key': api_key}
+    response = requests.get(api_url, params=params)
+    response.raise_for_status()
 
-    try:
+    images_data = response.json()[:count]
+
+    for index, image_data in enumerate(images_data, start=1):
+        image_name = image_data['image']
+        image_date = image_data['date']
+
+        date_only = image_date.split(' ')[0]
+        year, month, day = date_only.split('-')
+
         params = {'api_key': api_key}
-        response = requests.get(api_url, params=params)
-        response.raise_for_status()
-        images_data = response.json()[:count]
-
-        for index, image_data in enumerate(images_data, start=1):
-            image_name = image_data['image']
-            image_date = image_data['date']
-
-            date_only = image_date.split(' ')[0]
-            year, month, day = date_only.split('-')
-
-            image_url = (
-                f'https://api.nasa.gov/EPIC/archive/natural/'
-                f'{year}/{month}/{day}/png/{image_name}.png?api_key={api_key}'
-            )
-            try:
-                image_response = requests.get(image_url)
-                image_response.raise_for_status()
-
-                filename = f'nasa_epic_{index}.png'
-                save_path = os.path.join(folder_name, filename)
-                with open(save_path, 'wb') as file:
-                    file.write(image_response.content)
-
-            except requests.exceptions.RequestException as e:
-                print(f'Ошибка при скачивании изображения {image_url}: {e}')
-
-    except requests.exceptions.RequestException as e:
-        print(f'Ошибка при запросе API EPIC: {e}')
+        image_url = (
+            f'https://api.nasa.gov/EPIC/archive/natural/'
+            f'{year}/{month}/{day}/png/{image_name}.png'
+        )
+        image_url = requests.Request(
+            'GET', image_url, params=params
+        ).prepare().url
+        download_and_save_image('nasa_epic', image_url, folder_name, index)
 
 
 def main():
@@ -78,7 +69,16 @@ def main():
               'или переменную окружения NASA_API_KEY.')
         return
 
-    download_nasa_epic_images(api_key, args.folder_name, args.count)
+    try:
+        download_nasa_epic_images(api_key, args.folder_name, args.count)
+    except requests.exceptions.RequestException as req_err:
+        print(f"Ошибка при работе с API или сетью: {req_err}")
+    except (ValueError, KeyError) as json_err:
+        print(f"Ошибка обработки данных: {json_err}")
+    except OSError as file_err:
+        print(f"Ошибка при работе с файловой системой: {file_err}")
+    except Exception as unexpected_err:
+        print(f'Непредвиденная ошибка: {unexpected_err}')
 
 
 if __name__ == '__main__':
